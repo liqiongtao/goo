9c4018552400344bccc36b71ae32b67a3b30591b
liqiongtao <hnatao@126.com>
2020-12-16 20:26:22 +0800

    bugfix

--- context.go
+++ context.go
@@ -3,6 +3,8 @@ package goo
 import (
 	"context"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 var (
@@ -11,30 +13,31 @@ var (
 	Context     = ctx
 )
 
-// func init() {
-// 	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
-//
-// 	AsyncFunc(func() {
-// 		for si := range sig {
-// 			switch si {
-// 			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
-// 				cancel()
-// 			case syscall.SIGUSR1:
-// 			case syscall.SIGUSR2:
-// 			default:
-// 			}
-// 		}
-// 	})
-// }
-//
-// func SetBasePath(basePath string) {
-// 	ctx = context.WithValue(ctx, "basePath", basePath)
-// }
-//
-// func BasePath() string {
-// 	v := ctx.Value("basePath")
-// 	if v == nil {
-// 		return ""
-// 	}
-// 	return v.(string)
-// }
+func init() {
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
+
+	AsyncFunc(func() {
+		for si := range sig {
+			switch si {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				cancel()
+				break
+			case syscall.SIGUSR1:
+			case syscall.SIGUSR2:
+			default:
+			}
+		}
+	})
+}
+
+func SetBasePath(basePath string) {
+	ctx = context.WithValue(ctx, "basePath", basePath)
+}
+
+func BasePath() string {
+	v := ctx.Value("basePath")
+	if v == nil {
+		return ""
+	}
+	return v.(string)
+}
