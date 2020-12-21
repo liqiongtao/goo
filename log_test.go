4b5ab3260cd1ba98ad4b374e84bcbc81bdb4fb2e
liqiongtao <hnatao@126.com>
2020-12-21 11:55:29 +0800

    add log hook

--- log_test.go
+++ log_test.go
@@ -1,6 +1,8 @@
 package goo
 
 import (
+	"fmt"
+	"github.com/liqiongtao/goo/logger"
 	"sync"
 	"testing"
 )
@@ -17,4 +19,19 @@ func TestLogDebug(t *testing.T) {
 		Log.Info("this is info")
 	}()
 	wg.Wait()
+}
+
+func TestLogHook(t *testing.T) {
+	Log.AddHook(func(level logger.Level, buf []byte) {
+		fmt.Println("---log.hook begin-----")
+		fmt.Print(logger.LevelText[level], string(buf))
+		fmt.Println("---log.hook end-----")
+	})
+
+	Log.Debug("this is debug")
+	Log.WithField("name", "hnatao").Info("this is info")
+	Log.Warn("this is warn")
+	Log.Error("this is error")
+	Log.Panic("this is panic")
+	Log.Fatal("this is fatal")
 }
--- logger/file_logger.go
+++ logger/file_logger.go
@@ -6,6 +6,7 @@ import (
 
 type FileLogger struct {
 	adapter *FileAdapter
+	hooks   []func(level Level, buf []byte)
 }
 
 func NewFileLogger(filePath, fileName string) *FileLogger {
@@ -21,37 +22,41 @@ func NewFileLogger(filePath, fileName string) *FileLogger {
 	}
 }
 
+func (fl *FileLogger) AddHook(fn func(level Level, buf []byte)) {
+	fl.hooks = append(fl.hooks, fn)
+}
+
 func (fl *FileLogger) WithField(key string, value interface{}) *Logger {
-	l := &Logger{Adapter: fl.adapter}
+	l := &Logger{Adapter: fl.adapter, hooks: fl.hooks}
 	return l.WithField(key, value)
 }
 
 func (fl *FileLogger) Debug(v ...interface{}) {
-	l := &Logger{Adapter: fl.adapter}
+	l := &Logger{Adapter: fl.adapter, hooks: fl.hooks}
 	l.Debug(v...)
 }
 
 func (fl *FileLogger) Info(v ...interface{}) {
-	l := &Logger{Adapter: fl.adapter}
+	l := &Logger{Adapter: fl.adapter, hooks: fl.hooks}
 	l.Info(v...)
 }
 
 func (fl *FileLogger) Warn(v ...interface{}) {
-	l := &Logger{Adapter: fl.adapter}
+	l := &Logger{Adapter: fl.adapter, hooks: fl.hooks}
 	l.Warn(v...)
 }
 
 func (fl *FileLogger) Error(v ...interface{}) {
-	l := &Logger{Adapter: fl.adapter}
+	l := &Logger{Adapter: fl.adapter, hooks: fl.hooks}
 	l.Error(v...)
 }
 
 func (fl *FileLogger) Panic(v ...interface{}) {
-	l := &Logger{Adapter: fl.adapter}
+	l := &Logger{Adapter: fl.adapter, hooks: fl.hooks}
 	l.Panic(v...)
 }
 
 func (fl *FileLogger) Fatal(v ...interface{}) {
-	l := &Logger{Adapter: fl.adapter}
+	l := &Logger{Adapter: fl.adapter, hooks: fl.hooks}
 	l.Fatal(v...)
 }
