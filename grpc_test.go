acfd544982ceb89ec190cf0afdaca19a680a1ec7
liqiongtao <hnatao@126.com>
2020-12-22 10:26:03 +0800

    add test

--- grpc_test.go
+++ grpc_test.go
@@ -6,8 +6,16 @@ import (
 )
 
 func TestNewGRPCServer(t *testing.T) {
+	s := NewGRPCServer(15001)
+	if err := s.Serve(); err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func TestNewGRPCServer2(t *testing.T) {
 	s := NewGRPCServer(15001,
 		GRPCServiceName("test"),
+		GRPCConsul("127.0.0.1:18001", "test", "123456"),
 	)
 	if err := s.Serve(); err != nil {
 		log.Println(err.Error())
